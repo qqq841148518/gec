5fc9461a9c63c38cd8782760dc02483b79c409cf
gec <841148518@qq.com>
2021-01-29 11:16:00 +0800

    黑色删除

--- test/src/com/gec/test/Test1.java
+++ test/src/com/gec/test/Test1.java
@@ -4,9 +4,7 @@ public class Test1 {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.print("11");
-		System.out.print("芜湖");
-		System.out.println("qifei");
+		
 		System.out.println("黑色");
 	}
 
